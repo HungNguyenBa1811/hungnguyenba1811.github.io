cf718f73c829c97ebcc0c3edac0a58011a9d58ad
HungNguyenBa1811 <nahh1811@gmail.com>
2021-07-24 15:12:47 +0700

    hehe

--- Lesson_1/index.js
+++ Lesson_1/index.js
@@ -1,6 +1,10 @@
 var x = 0
 function yeah(){
-    x += 100000
+    x += 100000;
     window.location = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
     document.getElementById('money').innerHTML = '$' + x;
+}
+function yes(){
+    x+= 1000000;
+    document.getElementById('money').innerHTML = x + 'BTC';
 }
