2eaa94698d0b5a1a493b6661f64c4df3d679e0aa
unknown <nahh1811@gmail.com>
2021-08-18 17:32:30 +0700

    homework

--- Lesson_6/homework/index.js
+++ Lesson_6/homework/index.js
@@ -10,3 +10,12 @@ function redirect_3(){
 function cancel(){
     window.location = './index.html'
 }
+if(window.innerHeight > window.innerWidth || screen.availHeight > screen.availWidth){
+    document.getElementById('body').style.display = 'none';
+    setTimeout( function(){
+        alert("Please rotate your device or being rickrolled in 5 seconds");
+    }, 1234);
+    setTimeout( function(){
+        window.location = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+    }, 5000)
+}
