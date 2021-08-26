46c53bd450c1dd4b060f7a43ead0c96cc00eef9e
HungNguyenBa1811 <nahh1811@gmail.com>
2021-08-26 20:32:58 +0700

    preparing

---
+++ San Pham Cuoi Khoa/js/data.js
@@ -0,0 +1,20 @@
+let products = [
+    {
+        name: '',
+        brandname: 'Apple',
+        category: 'keyboard',
+    }
+    ,
+    {
+        name: '',
+        brandname: 'Lenovo',
+        category: 'mouse',
+    }
+    ,
+    {
+        name: '',
+        brandname: 'Nintendo',
+        category: 'handle'
+    }
+]
+export {products}
