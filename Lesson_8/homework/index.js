811943c2b60550a62614947d17f79b02b66bb5f1
unknown <nahh1811@gmail.com>
2021-08-30 10:49:45 +0700

    hell

--- Lesson_8/homework/index.js
+++ Lesson_8/homework/index.js
@@ -1,7 +1,15 @@
 // bài 1
-let a = document.querySelector('#a')
-let b = document.querySelector('#b')
-if(a == 0 && b == 0){
-    
-}
-let equation = (a, b) => - b / a;
+let a = document.getElementById('a');
+let b = document.getElementById('b');
+let equation = () => document.getElementById('result').innerHTML = 'Answer: ' + (-b / a) ;
+console.log(-b/a)
+// bài 3
+
+let foo_a = (x, y, z) => console.log(x, y, z);
+let foo_b = () => alert('Hello');
+let foo_c = (a, b) => a + b * 100;
+
+// bài 4
+fetch('./studentData.json')
+    .then(response => response.json())
+    .then(json => console.log(json))
