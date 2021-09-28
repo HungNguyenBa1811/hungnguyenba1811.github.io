657ad5061cf85717027113df1becc07aa06f7eca
unknown <nahh1811@gmail.com>
2021-09-28 22:33:59 +0700

    main char done 60%

--- San Pham Cuoi Khoa/js/index.js
+++ San Pham Cuoi Khoa/js/index.js
@@ -19,3 +19,33 @@ function carousel() {
     dots[slideIndex-1].className += " active-ball";
     setTimeout(carousel, 7654); // Change image every 7.654 seconds
 }
+
+let addToCartBtn = document.querySelectorAll(".add-to-cart");
+let showNumProduct = document.querySelector(".cart-item-count");
+let keys  = Object.keys(localStorage)
+let cart = []
+let numProduct = 0
+if(keys.length){
+    cart = JSON.parse(localStorage.getItem('cart'))
+    numProduct = localStorage.getItem('numProduct')
+}
+showNumProduct.innerHTML = numProduct;
+// Increse num product
+for (let x of addToCartBtn) {
+  x.onclick = () => {
+    ++numProduct;
+    showNumProduct.innerHTML = numProduct;
+    let product = {
+      name: "",
+      image: "",
+      price: "",
+    };
+    let productDom = x.parentElement.children;
+    product.name = productDom[2].innerHTML;
+    product.price = productDom[0].innerHTML;
+    product.image = ""
+    cart.push(product)
+    localStorage.setItem('cart',JSON.stringify(cart))
+    localStorage.setItem('numProduct',numProduct)
+  };
+}
