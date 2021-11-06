0daaa178ee70b2ce87133ffee64c151586d0cb27
unknown <nahh1811@gmail.com>
2021-11-06 15:06:06 +0700

    hell

--- JSI/Lesson_1/homework/index.js
+++ JSI/Lesson_1/homework/index.js
@@ -1,10 +1,10 @@
-let userApi = 'https://covid-19-data.p.rapidapi.com/country/code?code=it'
+let userApi = 'https://covid-19-data.p.rapidapi.com/totals'
 fetch(userApi, {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
 		"x-rapidapi-key": "ea05861deemshd05fd813b22a171p1e673cjsn04db873bf3a4"
-	} 
+	}
 })
     .then(function(response) {
         return response.json();
@@ -12,18 +12,14 @@ fetch(userApi, {
     .then(function(posts) {
         
         let html = posts.map(function(item) {
-            return `<li>
-				<h2>${item.country}</h2>
-				<h2>${item.code}</h2>
-                <h2>${item.confirmed}</h2>
-				<h2>${item.recovered}</h2>
-				<h2>${item.critical}</h2>
-				<p>${item.deaths}</p>
-				<h2>${item.latitude}</h2>
-				<h2>${item.longitude}</h2>
-				<h2>${item.lastChange}</h2>
-				<h2>${item.lastUpdate}</h2>
-            </li>`
+            return `<tr>
+                <td class="text-center">${item.confirmed}</td>
+				<td class="text-center">${item.recovered}</td>
+				<td class="text-center">${item.critical}</td>
+				<td class="text-center">${item.deaths}</td>
+				<td>${item.lastChange}</td>
+				<td>${item.lastUpdate}</td>
+            </tr>`
         });
         
         document.querySelector('.call').innerHTML = html;
