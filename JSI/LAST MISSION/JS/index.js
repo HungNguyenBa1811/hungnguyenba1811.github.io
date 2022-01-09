22028ec535128caaf21dda71ca22e1fa9552821b
unknown <nahh1811@gmail.com>
2022-01-09 16:17:04 +0700

    upload code

--- JSI/LAST MISSION/JS/index.js
+++ JSI/LAST MISSION/JS/index.js
@@ -22,3 +22,20 @@ toggleSystem("1")
 toggleSystem("2")
 toggleSystem("3")
 toggleSystem("4")
+// Scroll to top
+let scrollButton = document.querySelector("#back_top")
+let scroll_1 = document.querySelector('.header-container')
+scrollButton.addEventListener('click', function () {
+    scroll_1.scrollIntoView()
+})
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollButton.style.display = "block";
+    } else {
+      scrollButton.style.display = "none";
+    }
+}
