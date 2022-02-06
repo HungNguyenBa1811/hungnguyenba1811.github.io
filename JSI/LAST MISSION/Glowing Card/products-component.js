6122337445eb048c1bb56a45c15c3c59cd7aacd4
unknown <nahh1811@gmail.com>
2022-02-06 11:02:35 +0700

    90%

--- JSI/LAST MISSION/Glowing Card/products-component.js
+++ JSI/LAST MISSION/Glowing Card/products-component.js
@@ -1,103 +1,103 @@
-const style = `
-.card{
-    position: relative;
-}
+// const style = `
+// .card{
+//     position: relative;
+// }
 
-.card .face{
-    width: 300px;
-    height: 200px;
-    transition:.4s;
-}
+// .card .face{
+//     width: 300px;
+//     height: 200px;
+//     transition:.4s;
+// }
 
-.card .face.face1{
-    position: relative;
-    background: #333;
-    display: flex;
-    justify-content: center;
-    align-content:center;
-    align-items: center;
-    z-index: 1;
-    transform: translateY(100px);
-}
+// .card .face.face1{
+//     position: relative;
+//     background: #333;
+//     display: flex;
+//     justify-content: center;
+//     align-content:center;
+//     align-items: center;
+//     z-index: 1;
+//     transform: translateY(100px);
+// }
   
-.card:hover .face.face1{
-    transform: translateY(0);
-    box-shadow:
-        inset 0 0 60px whitesmoke,
-        inset 20px 0 80px #f0f,
-        inset -20px 0 80px #0ff,
-        inset 20px 0 300px #f0f,
-        inset -20px 0 300px #0ff,
-        0 0 50px #fff,
-        -10px 0 80px #f0f,
-        10px 0 80px #0ff;  
-}
+// .card:hover .face.face1{
+//     transform: translateY(0);
+//     box-shadow:
+//         inset 0 0 60px whitesmoke,
+//         inset 20px 0 80px #f0f,
+//         inset -20px 0 80px #0ff,
+//         inset 20px 0 300px #f0f,
+//         inset -20px 0 300px #0ff,
+//         0 0 50px #fff,
+//         -10px 0 80px #f0f,
+//         10px 0 80px #0ff;  
+// }
   
-.card .face.face1 .content{
-    opacity: .2;
-    transition:  0.5s;
-    text-align: center;
-}
+// .card .face.face1 .content{
+//     opacity: .2;
+//     transition:  0.5s;
+//     text-align: center;
+// }
   
-.card:hover .face.face1 .content{
-    opacity: 1; 
-}
+// .card:hover .face.face1 .content{
+//     opacity: 1; 
+// }
   
-.card .face.face1 .content i{
-    font-size: 3em;
-    color: white;
-    display: inline-block;
-}
+// .card .face.face1 .content i{
+//     font-size: 3em;
+//     color: white;
+//     display: inline-block;
+// }
   
-.card .face.face1 .content h3{
-    font-size: 1em;
-    color: white;
-    text-align: center;
-}
+// .card .face.face1 .content h3{
+//     font-size: 1em;
+//     color: white;
+//     text-align: center;
+// }
   
-.card .face.face1 .content a{
-    transition: .5s;
-}
+// .card .face.face1 .content a{
+//     transition: .5s;
+// }
   
-.card .face.face2{
-    position: relative;
-    background: whitesmoke;
-    display: flex;
-    font-size: 10px;
-    align-items: center;
-    justify-content: center;
-    padding: 20px;
-    box-sizing: border-box;
-    box-shadow: 0 20px 50px rgba(0,0,0,.8);
-    transform: translateY(-100px);
-}
+// .card .face.face2{
+//     position: relative;
+//     background: whitesmoke;
+//     display: flex;
+//     font-size: 10px;
+//     align-items: center;
+//     justify-content: center;
+//     padding: 20px;
+//     box-sizing: border-box;
+//     box-shadow: 0 20px 50px rgba(0,0,0,.8);
+//     transform: translateY(-100px);
+// }
   
-.card:hover .face.face2{
-    transform: translateY(0);
-}
+// .card:hover .face.face2{
+//     transform: translateY(0);
+// }
   
-.card .face.face2 .content p, a{
-    font-size: 10pt;
-    margin: 0 ;
-    padding: 0;
-    color:#333;
-}
+// .card .face.face2 .content p, a{
+//     font-size: 10pt;
+//     margin: 0 ;
+//     padding: 0;
+//     color:#333;
+// }
   
-.card .face.face2 .content a{
-    text-decoration:none;
-    color: black;
-    box-sizing: border-box;
-    outline : 1px dashed #333;
-    padding: 10px;
-    margin: 15px 0 0;
-    display: inline-block;
-}
+// .card .face.face2 .content a{
+//     text-decoration:none;
+//     color: black;
+//     box-sizing: border-box;
+//     outline : 1px dashed #333;
+//     padding: 10px;
+//     margin: 15px 0 0;
+//     display: inline-block;
+// }
   
-.card .face.face2 .content a:hover{
-    background: #333 ;
-    color: whitesmoke; 
-    box-shadow: inset 0px 0px 10px rgba(0,0,0,0.5);
-}`
+// .card .face.face2 .content a:hover{
+//     background: #333 ;
+//     color: whitesmoke; 
+//     box-shadow: inset 0px 0px 10px rgba(0,0,0,0.5);
+// }`
 class ProductsCard extends HTMLElement {
     constructor(){
         super();
@@ -108,9 +108,6 @@ class ProductsCard extends HTMLElement {
         this.productSaleOff = this.getAttribute("productSaleOff")
         this.productSaleOffValue = this.getAttribute("productSaleOffValue")
         this._shadowDom.innerHTML = `
-            <style>
-                ${style}
-            </style>
             <div class="card">
 
                 <div class="face face1">
